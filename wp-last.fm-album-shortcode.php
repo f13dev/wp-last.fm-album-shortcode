30afea70892e7662661bd697abfa14491c0aa80b
James Valentine <jv@f13dev.com>
2016-07-19 10:02:45 +0100

    Creation of plugin PHP file and wordpress header

---
+++ wp-last.fm-album-shortcode.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: F13 Last.fm album Shortcode
+Plugin URI: http://f13dev.com/wordpress-plugin-lastfm-album-shortcode/
+Description: Embed information about a music album into a page or blog post using shortcode.
+Version: 1.0
+Author: Jim Valentine - f13dev
+Author URI: http://f13dev.com
+Text Domain: f13-lastfm-album-shortcode
+License: GPLv3
+*/
+
+/*
+Copyright 2016 James Valentine - f13dev (jv@f13dev.com)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
